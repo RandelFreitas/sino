8230515d013e6d5e9c70414b134199c485ba8b80
Matheus Oliveira <matheusoliveira2496@gmail.com>
2020-03-31 15:46:28 -0300

    Removendo rota de criação de usuário (O user será criado no momento da criação do funcionário/gestor)

--- BACK/src/controllers/AuthController.js
+++ BACK/src/controllers/AuthController.js
@@ -6,19 +6,6 @@ const jwt = require("jsonwebtoken");
 const AuthConfig = require("../config/auth");
 
 module.exports = {
-    async register(req, res){
-        const {email} = req.body;
-        try{
-            if(await User().findOne({email})){
-                return res.status(400).send({error: 'User already exists: '+email})
-            }
-            const user = await User().create(req.body);
-            user.password = undefined;
-            return res.json(user);
-        } catch (err) {
-            return res.status(400).send({error: 'Registration failed: '+err});
-        }
-    },
     async authenticate(req, res){
         const {email, password} = req.body;
         try{
--- BACK/src/routes/AuthRouter.js
+++ BACK/src/routes/AuthRouter.js
@@ -2,9 +2,6 @@ const express = require("express");
 const routes = express.Router();
 const AuthController = require("../controllers/AuthController") 
 
-//create user
-routes.post("/register", AuthController.register);
-
 //authenticate user
 routes.post("/authenticate", AuthController.authenticate);
 
