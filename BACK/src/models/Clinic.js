cdf11a58a2e786f795765e49d41fc3c169b93e7e
Matheus Oliveira <matheusoliveira2496@gmail.com>
2020-04-01 22:09:05 -0300

    Criação do Model Clinic

---
+++ BACK/src/models/Clinic.js
@@ -0,0 +1,30 @@
+const mongoose = require("../database/Connect");
+const mongoosePaginate = require("mongoose-paginate");
+const { tenantlessModel } = require("../middleware/MultiTenant");
+const Address = require('./Address');
+
+const ClinicSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  cnpj: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String
+  },
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address',
+  },
+});
+
+ClinicSchema.plugin(mongoosePaginate);
+const Clinic = tenantlessModel("Clinic", ClinicSchema);
+module.exports = Clinic;
