5ea92c63bce56322688ddd5152d811fac2ec09c2
randel <randelfreitas@gmail.com>
2020-04-07 18:56:10 -0300

    Correcao autenticacao Front

--- FRONT/src/services/auth.js
+++ FRONT/src/services/auth.js
@@ -1,6 +1,6 @@
 export const TOKEN_KEY = "@token";
-export const isAuthenticated = () => true;
-//export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+//export const isAuthenticated = () => true;
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
 export const login = token => {
