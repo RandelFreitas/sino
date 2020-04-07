0982bfd235b389e009d8302f28ec101a69c9faa0
Matheus Oliveira <matheusoliveira2496@gmail.com>
2020-04-07 16:36:01 -0300

    instalando cors

--- BACK/server.js
+++ BACK/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const requireDir = require("require-dir");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 const { bindCurrentNamespace } = require("./src/middleware/Storage");
